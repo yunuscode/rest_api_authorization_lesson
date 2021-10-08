7deb3d590341c48efe6a0b828bbd28d6d52d8710
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-10-08 19:41:42 +0500

    add user schema

---
+++ src/models/UserModel.js
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+	{
+		name: {
+			type: String,
+			min: 2,
+			max: 32,
+			required: true,
+		},
+		username: {
+			type: String,
+			lowercase: true,
+			required: true,
+			unique: true,
+			min: 5,
+			max: 16,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: {
+			createdAt: "created_at",
+			updatedAt: "updated_at",
+		},
+	}
+);
+
+const User = new model("User", UserSchema);

170665c5cf72c31311e3ee17d535926860ba5b58
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-10-08 20:21:44 +0500

    Add signup controller

--- src/controllers/User/UserRouteController.js
+++ src/controllers/User/UserRouteController.js
@@ -1,3 +1,7 @@
+import User from "../../models/UserModel.js";
+import Validations from "../../modules/validations.js";
+import { genCrypt } from "../../modules/bcrypt.js";
+
 export default class UserRouteController {
 	static async UserHomeGetController(request, response, next) {
 		try {
@@ -6,6 +10,27 @@ export default class UserRouteController {
 				message: "OK",
 			});
 		} catch (error) {
+			next(error);
+		}
+	}
+	static async UserSignUpPostController(request, response, next) {
+		try {
+			const { username, name, password } =
+				await Validations.SignUpValidation(request.body);
+
+			const user = await User.create({
+				name,
+				username,
+				password: await genCrypt(password),
+			});
+
+			// console.log(user);
+
+			// console.log(username, name, password);
+		} catch (error) {
+			if (error.name === "MongoServerError" && error.code === 11000) {
+				next(new Error("Username must be unique"));
+			}
 			next(error);
 		}
 	}

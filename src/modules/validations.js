0b65c6877735606d6b28780f39a2057e832352b0
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-10-08 20:21:54 +0500

    Add signup

--- src/modules/validations.js
+++ src/modules/validations.js
@@ -1,3 +1,22 @@
 import Joi from "joi";
 
-export default class Validations {}
+export default class Validations {
+	static async SignUpValidation(data) {
+		return await Joi.object({
+			name: Joi.string()
+				.min(2)
+				.max(32)
+				.required()
+				.trim()
+				.error(new Error("Name is invalid")),
+			username: Joi.string()
+				.required()
+				.regex(/^(?=.{5,16}$)(?![_])(?!.*[_]{2})[a-zA-Z0-9_]+(?<![_])$/)
+				.error(new Error("Username is invalid")),
+			password: Joi.string()
+				.required()
+				.min(4)
+				.error(new Error("Password is invalid")),
+		}).validateAsync(data);
+	}
+}
--- src/routes/User/UserRoute.js
+++ src/routes/User/UserRoute.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import UserRouteController from "../../controllers/User/UserRouteController.js";
+import User from "../../models/UserModel.js";
 
 const UserRoute = Router();
 
+UserRoute.post("/sign_up", UserRouteController.UserSignUpPostController);
 UserRoute.get("/", UserRouteController.UserHomeGetController);
 
 export default UserRoute;
